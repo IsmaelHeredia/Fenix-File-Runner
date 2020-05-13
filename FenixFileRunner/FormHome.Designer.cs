386304056224fd0a24b1b78e7e5fd61fa6746215
IsmaelHeredia <ismaelheredia16@gmail.com>
2020-05-13 17:29:01 -0300

    Version 1.0

---
+++ FenixFileRunner/FormHome.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace FenixFileRunner
+{
+    partial class FormHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHome));
+            this.btnAddProgram = new Telerik.WinControls.UI.RadButton();
+            this.telerikMetroTheme = new Telerik.WinControls.Themes.TelerikMetroTheme();
+            this.gbEnterProgram = new Telerik.WinControls.UI.RadGroupBox();
+            this.btnLoadProgram = new Telerik.WinControls.UI.RadButton();
+            this.txtProgram = new Telerik.WinControls.UI.RadTextBox();
+            this.btnDeleteProgram = new Telerik.WinControls.UI.RadButton();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAddProgram)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gbEnterProgram)).BeginInit();
+            this.gbEnterProgram.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLoadProgram)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtProgram)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnDeleteProgram)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddProgram
+            // 
+            this.btnAddProgram.Location = new System.Drawing.Point(71, 118);
+            this.btnAddProgram.Name = "btnAddProgram";
+            this.btnAddProgram.Size = new System.Drawing.Size(118, 24);
+            this.btnAddProgram.TabIndex = 1;
+            this.btnAddProgram.Text = "Add program";
+            this.btnAddProgram.ThemeName = "TelerikMetro";
+            this.btnAddProgram.Click += new System.EventHandler(this.btnAddProgram_Click);
+            // 
+            // gbEnterProgram
+            // 
+            this.gbEnterProgram.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.gbEnterProgram.Controls.Add(this.btnLoadProgram);
+            this.gbEnterProgram.Controls.Add(this.txtProgram);
+            this.gbEnterProgram.HeaderText = "Enter program";
+            this.gbEnterProgram.Location = new System.Drawing.Point(12, 21);
+            this.gbEnterProgram.Name = "gbEnterProgram";
+            this.gbEnterProgram.Size = new System.Drawing.Size(393, 75);
+            this.gbEnterProgram.TabIndex = 2;
+            this.gbEnterProgram.Text = "Enter program";
+            this.gbEnterProgram.ThemeName = "TelerikMetro";
+            // 
+            // btnLoadProgram
+            // 
+            this.btnLoadProgram.Location = new System.Drawing.Point(326, 33);
+            this.btnLoadProgram.Name = "btnLoadProgram";
+            this.btnLoadProgram.Size = new System.Drawing.Size(52, 24);
+            this.btnLoadProgram.TabIndex = 1;
+            this.btnLoadProgram.Text = "...";
+            this.btnLoadProgram.ThemeName = "TelerikMetro";
+            this.btnLoadProgram.Click += new System.EventHandler(this.btnLoadProgram_Click);
+            // 
+            // txtProgram
+            // 
+            this.txtProgram.AllowDrop = true;
+            this.txtProgram.Location = new System.Drawing.Point(20, 35);
+            this.txtProgram.Name = "txtProgram";
+            this.txtProgram.ReadOnly = true;
+            this.txtProgram.Size = new System.Drawing.Size(300, 24);
+            this.txtProgram.TabIndex = 0;
+            this.txtProgram.ThemeName = "TelerikMetro";
+            this.txtProgram.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtProgram_DragDrop);
+            this.txtProgram.DragEnter += new System.Windows.Forms.DragEventHandler(this.txtProgram_DragEnter);
+            // 
+            // btnDeleteProgram
+            // 
+            this.btnDeleteProgram.Location = new System.Drawing.Point(214, 118);
+            this.btnDeleteProgram.Name = "btnDeleteProgram";
+            this.btnDeleteProgram.Size = new System.Drawing.Size(118, 24);
+            this.btnDeleteProgram.TabIndex = 3;
+            this.btnDeleteProgram.Text = "Delete program";
+            this.btnDeleteProgram.ThemeName = "TelerikMetro";
+            this.btnDeleteProgram.Click += new System.EventHandler(this.btnDeleteProgram_Click);
+            // 
+            // FormHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 161);
+            this.Controls.Add(this.btnDeleteProgram);
+            this.Controls.Add(this.gbEnterProgram);
+            this.Controls.Add(this.btnAddProgram);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormHome";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "Fenix File Runner 1.0 - Copyright © Ismael Heredia 2020";
+            this.ThemeName = "TelerikMetro";
+            ((System.ComponentModel.ISupportInitialize)(this.btnAddProgram)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gbEnterProgram)).EndInit();
+            this.gbEnterProgram.ResumeLayout(false);
+            this.gbEnterProgram.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnLoadProgram)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtProgram)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnDeleteProgram)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Telerik.WinControls.UI.RadButton btnAddProgram;
+        private Telerik.WinControls.Themes.TelerikMetroTheme telerikMetroTheme;
+        private Telerik.WinControls.UI.RadGroupBox gbEnterProgram;
+        private Telerik.WinControls.UI.RadButton btnLoadProgram;
+        private Telerik.WinControls.UI.RadTextBox txtProgram;
+        private Telerik.WinControls.UI.RadButton btnDeleteProgram;
+    }
+}
---
+++ FenixFileRunner/FormHome.cs
@@ -0,0 +1,115 @@
+﻿// Fenix File Runner 1.0
+// Copyright © Ismael Heredia 2020
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+using Telerik.WinControls;
+using Telerik.WinControls.UI;
+
+namespace FenixFileRunner
+{
+    public partial class FormHome : Telerik.WinControls.UI.RadForm
+    {
+        public string program_title;
+
+        public FormHome()
+        {
+            InitializeComponent();
+            program_title = "Fenix File Runner 1.0";
+            RadMessageBox.SetThemeName("TelerikMetro");
+        }
+
+        private void btnAddProgram_Click(object sender, EventArgs e)
+        {
+            string program = txtProgram.Text;
+            string name = Path.GetFileNameWithoutExtension(program);
+            string value = program;
+            if (program != "")
+            {
+                try
+                {
+                    RegistryKey registry = Registry.LocalMachine;
+                    registry = registry.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                    registry.CreateSubKey(name);
+                    registry.SetValue(name, value, RegistryValueKind.String);
+                    registry.Close();
+                    RadMessageBox.Show("Program added in the registry", program_title, MessageBoxButtons.OK, RadMessageIcon.Info, MessageBoxDefaultButton.Button1);
+                }
+                catch
+                {
+                    RadMessageBox.Show("Error adding program in registry", program_title, MessageBoxButtons.OK, RadMessageIcon.Error, MessageBoxDefaultButton.Button1);
+                }
+            }
+            else
+            {
+                RadMessageBox.Show("Select program", program_title, MessageBoxButtons.OK, RadMessageIcon.Info, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void btnDeleteProgram_Click(object sender, EventArgs e)
+        {
+            string program = txtProgram.Text;
+            string name = Path.GetFileNameWithoutExtension(program);
+            if (program != "")
+            {
+                try
+                {
+                    RegistryKey registry = Registry.LocalMachine;
+                    registry = registry.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                    registry.DeleteValue(name);
+                    registry.Close();
+                    RadMessageBox.Show("Program deleted in the registry", program_title, MessageBoxButtons.OK, RadMessageIcon.Info, MessageBoxDefaultButton.Button1);
+                }
+                catch
+                {
+                    RadMessageBox.Show("Error deleting program in registry", program_title, MessageBoxButtons.OK, RadMessageIcon.Error, MessageBoxDefaultButton.Button1);
+                }
+            }
+            else
+            {
+                RadMessageBox.Show("Select program", program_title, MessageBoxButtons.OK, RadMessageIcon.Info, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void btnLoadProgram_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Directory.GetCurrentDirectory();
+            openFileDialog.Title = "Select program";
+            openFileDialog.DefaultExt = "exe";
+            openFileDialog.Filter = "Program (.exe)|*.exe";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtProgram.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void txtProgram_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files != null && files.Length != 0)
+            {
+                txtProgram.Text = files[0];
+            }
+        }
+
+        private void txtProgram_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+    }
+}
